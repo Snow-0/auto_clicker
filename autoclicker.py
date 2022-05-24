742fbd3e8d1c5d1ee7dad8f9db5dbd6c59e40628
Max <max@max.com>
2022-05-24 09:20:15 -0400

    added double click and repeat until stop function

--- autoclicker.py
+++ autoclicker.py
@@ -1,18 +1,50 @@
 import pyautogui as py
+import time
+import keyboard
 
 py.FAILSAFE = True
 
 
 class AutoClicker:
-    def __init__(self, single_click=1, mouse_button="Left", interval=0.25):
-        self.click = single_click
+    def __init__(self, repeat, mouse_button, interval, x_pos, y_pos):
+        self.repeat = repeat  # number of times to click
         self.interval = interval  # in seconds format
         if mouse_button == "Right":
             self.mouse_button = False  # True left click and false for right click
         self.mouse_button = True
+        self.x_pos = x_pos
+        self.y_pos = y_pos
 
-    def mouse_click(self, x_pos=None, y_pos=None):
-        if self.mouse_button:
-            py.click(x_pos, y_pos, self.click, self.interval)
-        else:
-            py.rightClick(x_pos, y_pos, self.click, self.interval)
+    def single_click(self):
+        time.sleep(3)
+        while self.repeat > 0:
+            if self.mouse_button:
+                py.click(x=self.x_pos, y=self.y_pos, interval=self.interval)
+                time.sleep(0.01)
+            if not self.mouse_button:
+                py.rightClick(x=self.x_pos, y=self.y_pos, interval=self.interval)
+                time.sleep(0.01)
+            if keyboard.is_pressed("q"):
+                break
+            self.repeat -= 1
+
+    def double_click(self):
+        time.sleep(3)
+        while self.repeat > 0:
+            if self.mouse_button:
+                py.doubleClick(x=self.x_pos, y=self.y_pos, interval=self.interval)
+                time.sleep(0.01)
+            if not self.mouse_button:
+                py.doubleClick(x=self.x_pos, y=self.y_pos, interval=self.interval, button="Right")
+                time.sleep(0.01)
+            if keyboard.is_pressed("q"):
+                break
+            self.repeat -= 1
+
+    def repeat_until_stop(self):
+        time.sleep(3)
+        while True:
+            py.click(x=self.x_pos, y=self.y_pos, interval=self.interval)
+            time.sleep(0.02)
+            if keyboard.is_pressed("q"):
+                break
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from autoclicker import AutoClicker
 from tkinter import *
 import tkinter as ttk
-import time
 
 root = Tk()
 root.title("Auto Clicker")
-root.geometry('500x400')
+root.geometry('530x400')
+
+# get the current item selected between the radio buttons
+var_1 = StringVar()
+var = StringVar()
 
 
 def main():
@@ -13,6 +16,13 @@ def main():
     repeat = int(repeat_entry.get())
     click_type = clicked.get()
     mouse_button = buttons.get()
+    try:
+        x_cursor_pos = int(x_entry.get())
+        y_cursor_pos = int(y_entry.get())
+    except ValueError:
+        x_cursor_pos = None
+        y_cursor_pos = None
+
     time_list_keys = ["hours", "minutes", "seconds", "milliseconds"]
     sum_time = 0
     time_dict = {time_list_keys[i]: int(root.entries[i].get()) for i in range(len(time_list_keys))}
@@ -26,9 +36,14 @@ def main():
             sum_time += value / 1000
         else:
             sum_time += value
-    # click = AutoClicker()
-    # time.sleep(5)
-    print(mouse_button)
+    mouse = AutoClicker(repeat, mouse_button, sum_time, x_cursor_pos, y_cursor_pos)
+    # determine if user chooses a single or double click
+    if click_type == "Double click":
+        mouse.double_click()
+    if click_type == "Single click" and var.get() != "Repeat":
+        mouse.repeat_until_stop()
+    else:
+        mouse.single_click()
 
 
 def show():
@@ -36,9 +51,17 @@ def show():
 
 
 def select_button():
-    selection = var.get()
+    selection = var_1.get()
     label.config(text=selection)
     return selection
+
+
+def select_radio_button():
+    selection = var_1.get()
+    repeat_selection = var.get()
+    x_entry.config(state=ttk.DISABLED if selection == "Current location" else ttk.NORMAL)
+    y_entry.config(state=ttk.DISABLED if selection == "Current location" else ttk.NORMAL)
+    repeat_entry.config(state=ttk.DISABLED if repeat_selection == "Repeat until stopped" else ttk.NORMAL)
 
 
 # create frame widgets
@@ -68,9 +91,12 @@ for interval_entry in interval_entries:
     grid_column += 1
 
 # Creates widgets for the click option frame
-repeat_label = ttk.Label(frame_click_options, text="Repeat", justify=LEFT, anchor="w")
+repeat_button = ttk.Radiobutton(frame_click_options, text="Repeat", justify=LEFT, anchor="w", variable=var,
+                                value="Repeat", command=select_radio_button)
 repeat_entry = ttk.Entry(frame_click_options, width=5, justify=CENTER)
 repeat_entry.insert(0, "1")
+until_stop_button = ttk.Radiobutton(frame_click_options, text="Repeat until stopped", justify=LEFT, anchor="w",
+                                    variable=var, value="Repeat until stopped", command=select_radio_button)
 time_label = ttk.Label(frame_click_options, text="times")
 click_type_label = ttk.Label(frame_click_options, text="Click type: ")
 mouse_button_label = ttk.Label(frame_click_options, text="Mouse Button: ")
@@ -78,7 +104,7 @@ mouse_button_label = ttk.Label(frame_click_options, text="Mouse Button: ")
 # creating drop down menus for click options and button options
 click_options = ["Single click", "Double click"]
 clicked = StringVar()
-clicked.set("Single Click")
+clicked.set("Single click")
 click_drop = OptionMenu(frame_click_options, clicked, *click_options)
 
 button_options = ["Left", "Right"]
@@ -87,21 +113,21 @@ buttons.set("Left")
 button_drop = OptionMenu(frame_click_options, buttons, *button_options)
 
 # places the widgets
-repeat_label.grid(column=0, row=0, padx=3, pady=10, sticky=W)
+repeat_button.grid(column=0, row=0, padx=3, pady=10, sticky=W)
 repeat_entry.grid(column=1, row=0, padx=3, pady=10)
 time_label.grid(column=2, row=0, padx=4, pady=10)
-click_type_label.grid(column=0, row=1, padx=4, pady=10, sticky=W)
-mouse_button_label.grid(column=0, row=2, padx=4, pady=10, sticky=W)
-click_drop.grid(column=1, row=1, sticky=E)
-button_drop.grid(column=1, row=2, sticky=E)
 
-var = StringVar()
+click_type_label.grid(column=0, row=2, padx=4, pady=10, sticky=W)
+mouse_button_label.grid(column=0, row=3, padx=4, pady=10, sticky=W)
+click_drop.grid(column=1, row=2, sticky=E)
+button_drop.grid(column=1, row=3, sticky=E)
+until_stop_button.grid(column=0, row=1, padx=3, pady=10, sticky=W)
 
 # creates widgets for the cursor position frame
-current_location_radio = ttk.Radiobutton(frame_cursor_pos, text="Current location", variable=var,
-                                         value="Current location")
-pick_location_radio = ttk.Radiobutton(frame_cursor_pos, text="Pick location", justify=LEFT, anchor="w", variable=var,
-                                      value="Pick location")
+current_location_radio = ttk.Radiobutton(frame_cursor_pos, text="Current location", variable=var_1,
+                                         value="Current location", command=select_radio_button)
+pick_location_radio = ttk.Radiobutton(frame_cursor_pos, text="Pick location", justify=LEFT, anchor="w", variable=var_1,
+                                      value="Pick location", command=select_radio_button)
 x_pos_label = ttk.Label(frame_cursor_pos, text="X", padx=10, justify=LEFT, anchor="w")
 y_pos_label = ttk.Label(frame_cursor_pos, text="Y", padx=10, justify=LEFT, anchor="w")
 x_entry = ttk.Entry(frame_cursor_pos, width=5)
@@ -118,11 +144,11 @@ y_entry.grid(column=1, row=3, ipadx=5, ipady=5, padx=3, pady=10, sticky=W)
 # start and stop buttons
 
 start = ttk.Button(text="Start", command=main, justify=LEFT)
-stop = ttk.Button(text="Stop (q)")
+stop = ttk.Label(text="hold q to stop or move cursor to top left corner")
 quit_app = ttk.Button(text="quit", command=root.destroy)
 
 start.grid(column=0, row=2, padx=35, sticky=W)
-stop.grid(column=0, row=2, pady=17, sticky=S)
-quit_app.grid(column=0, row=2, sticky=E, padx=35)
+stop.grid(column=0, row=3, pady=17, sticky=S)
+quit_app.grid(column=0, row=2, sticky=S)
 
 root.mainloop()
